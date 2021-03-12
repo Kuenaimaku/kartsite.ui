9a68ab2b96ac25616cb0fab410e19c140824ccf2
kuenaimaku <--replace-all>
2021-03-12 03:33:48 -0600

    fix bulma import

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './../node_modules/Bulma/bulma.sass'
+import 'bulma/css/bulma.css'
 import '@mdi/font/css/materialdesignicons.css'
 
 createApp(App).mount('#app')
